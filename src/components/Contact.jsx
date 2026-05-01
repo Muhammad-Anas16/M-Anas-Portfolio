09b32022aaa0059009725b31663d2282dfa338e8
Muhammad-Anas16 <anas.hanf38@gmail.com>
2026-05-01 09:54:59 +0500

    ipdate a bit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,10 +6,10 @@ import { CiLinkedin } from "react-icons/ci";
 const Contact = () => {
   return (
     <div className="w-full max-w-4xl mx-auto px-4 py-8">
-      <div className="flex items-center gap-4">
+      {/* <div className="flex items-center gap-4">
         <h1 className="text-3xl font-bold text-white">Contact Me</h1>
         <div className="h-[1px] flex-1 bg-gradient-to-r from-white/20 to-transparent" />
-      </div>
+      </div> */}
       <div className="flex flex-col items- gap-4 mt-6">
         <h1>open to opportunities.</h1>
         <h4>Feel free to reach out!</h4>
